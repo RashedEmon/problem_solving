9cae5c324fd18d5a0e1462a9376365a083ea2ec6
Md. Rashedul Islam <68527347+RashedEmon@users.noreply.github.com>
2022-05-11 00:11:30 +0600

    Create expected_place_of_an_element_binary_search.py

---
+++ expected_place_of_an_element_binary_search.py
@@ -0,0 +1,16 @@
+def solve(arr,target):
+    mid=0
+    left=0
+    right=len(arr)-1
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==target:
+            return arr[mid]
+        elif arr[mid]>target:
+            right=mid-1
+        else:
+            left=mid+1
+    return mid+1
+    
+if __name__ == "__main__":
+    print(solve([1,2,3,6,7,9,10],5))
