b068eeef73c2fa5dbe61e54c99a47f654ff7592d
Md. Rashedul Islam <68527347+RashedEmon@users.noreply.github.com>
2024-09-28 14:02:12 +0600

    Update 496. next-greater-element-i.py

--- 496. next-greater-element-i.py
+++ 496. next-greater-element-i.py
@@ -1,7 +1,13 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
-        nums1_hashmap = {v:i for i, v in enumerate(nums1)}
+        '''
+        Prepare a hashmap of nums1 where the key will be the value and the index will be the value of the hashmap.
+        Initialize the result list with -1. Length of result list same as nums1.
+        First, iterate through nums2 and push the number into the stack until it finds a number greater than the previous numbers.
+        When it finds a greater number, it will start popping and check that the num is present in the nums1 using nums1_hashmap(number vs index mapping).
+        If it finds in nums1 then it will get the index and replace the greater element into the index of the result list.
+        '''
+        nums1_hashmap = {num:index for index, num in enumerate(nums1)}
         
         stack = []
         result = [-1 for _ in nums1]
